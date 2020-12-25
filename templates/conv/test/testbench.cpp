6ba106772d4ce4023009abe31d810097008bd3ca
Daniel Rauch <drauch@luigi.ece.utexas.edu>
2020-12-25 11:29:43 -0600

    conv testbench now working for at least one case

--- templates/conv/test/testbench.cpp
+++ templates/conv/test/testbench.cpp
@@ -17,6 +17,9 @@ void conv_golden (
 
 int main() {
 
+   printf("Beginning functional validation.\n");
+   fflush(stdout);
+
    // Create random inputs.
    data_t ifmaps[INPUT_RAM_SIZE];
    for (int i = 0; i < INPUT_RAM_SIZE; i++) {
@@ -34,18 +37,27 @@ int main() {
    // Run the golden function and the synthesized function with the same inputs.
    data_t ofmaps_synth[OUTPUT_RAM_SIZE];
    data_t ofmaps_gold[OUTPUT_RAM_SIZE];
+   printf("Running synthesized function...\n");
+   fflush(stdout);
    $lname(ifmaps, ofmaps_synth, filters);
+   printf("Running golden comparison function...\n");
+   fflush(stdout);
    conv_golden(ifmaps, ofmaps_gold, filters);
+
+   printf("Comparing expected vs. actual values.\n");
+   fflush(stdout);
 
    // Make sure they are the same.
    for (int k = 0; k < OUTPUT_RAM_SIZE; k++) {
       // Due to floating point rounding error the values might not be exactly
-      // the same. Make sure they are within 0.005 of each other.
-      if (abs(ofmaps_synth[k] - ofmaps_gold[k]) > 0.005) {
+      // the same. Make sure they are either within 0.01 or 1% within each other
+      float abs_error = abs(ofmaps_synth[k] - ofmaps_gold[k]);
+      float pct_error = abs_error / abs(ofmaps_gold[k]);
+      if (abs_error > 0.01 && pct_error > 0.01) {
          printf("MISMATCH between expected and actual values.\n");
          printf("k=%d\n", k);
-         printf("Expected = %f\n", ofmaps_gold[k]);
-         printf("Actual   = %f\n", ofmaps_synth[k]);
+         printf("Expected = %f\n", (float)(ofmaps_gold[k]));
+         printf("Actual   = %f\n", (float)(ofmaps_synth[k]));
          return -1;
       }
    }
